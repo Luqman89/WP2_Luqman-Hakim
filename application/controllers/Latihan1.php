a35aef41d82e0755fe077ce3abc4e29c918a29d7
Luqman89 <luqmnhakim3@gmail.com>
2024-03-29 06:25:12 +0700

    Pertemuan 1

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,15 @@
+<?php
+
+class Latihan1 extends CI_Controller{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+    $this->load->model('Model_latihan1');
+    $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+    echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = ". $hasil;
+    }
+}
---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input Mata Kuliah</title>
+</head>
+<body>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak')?>" method="post">
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form Input Data Mata Kuliah
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+</html>
