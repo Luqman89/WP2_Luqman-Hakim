d3dcf260fc999475850e70cac9351721e0fa6460
Luqman89 <luqmnhakim3@gmail.com>
2024-03-29 10:11:01 +0700

    menambahkan  about untuk rental buku pada pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()
+    ?>assets/stylebuku.css">
+</head>
+<body>
+    <section>
+        <h1><?php echo $judul?></h1>
+        <h4>Nama</h4>
+        <ul type="disc">
+            <li>Nama Depan : Luqman</li>
+            <li>Nama Belakang : Hakim</li>
+        </ul>
+        <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li>Jl.raya caringin No.199 Kota Bandung</li>
+        </ul>
+
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>Bandung</li>
+        </ul>
+
+        <h4>Oalahraga Favorit</h4>
+        <ul type="square">
+            <li>Badminton</li>
+        </ul>
+    </section>
+</body>
+</html>
